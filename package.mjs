a475e56a995fc43e65b92472d50f1e44a6d2ae33
Ralf S. Engelschall <rse@engelschall.com>
2023-09-09 23:49:55 +0200

    add support for pre-packaged distribution files

---
+++ package.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env zx
+
+const dst = "dist"
+
+await $`rm -rf ${dst}`
+await $`mkdir ${dst}`
+
+await $`cp -p analogclock.{js,css} ${dst}/`
+
+let html = fs.readFileSync("index.html", "utf8")
+html = html.replace(/basedir: ".+?"/g, "basedir: \".\"")
+html = html.replace(/node_modules\/.*?\/([^/]+")/g, "$1")
+fs.writeFileSync(`${dst}/index.html`, html, "utf8")
+
+await $`cp -rp node_modules/typopro-web/web/TypoPRO-SourceSansPro/* ${dst}/`
+await $`cp -p node_modules/jquery/dist/jquery.min.js ${dst}/`
+await $`cp -p node_modules/animejs/lib/anime.min.js ${dst}/`
+await $`cp -p node_modules/@svgdotjs/svg.js/dist/svg.min.js ${dst}/`
+await $`cp -p node_modules/svg.path.js/svg.path.js ${dst}/`
+await $`cp -p node_modules/moment/min/moment.min.js ${dst}/`
+await $`cp -p node_modules/mousetrap/mousetrap.min.js ${dst}/`
+await $`cp -p node_modules/howler/dist/howler.min.js ${dst}/`
+await $`cp -p node_modules/@rse/soundfx/soundfx.{browser.js,data*} ${dst}/`
+await $`cp -p node_modules/@rse/soundvm/soundvm.{browser.js,data*} ${dst}/`
+
