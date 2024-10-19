d1960533ad6f45ac4679751833d488b0266d5124
Ralf S. Engelschall <rse@engelschall.com>
2024-10-19 22:43:42 +0200

    update year in copyright messages

--- README.md
+++ README.md
@@ -169,7 +169,7 @@ CentralControl or ShowCockpit:
 License
 -------
 
-Copyright (c) 2021-2023 Dr. Ralf S. Engelschall (http://engelschall.com/)
+Copyright (c) 2021-2024 Dr. Ralf S. Engelschall (http://engelschall.com/)
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
--- analogclock.css
+++ analogclock.css
@@ -1,6 +1,6 @@
 /*
 **  AnalogClock ~ Analog Clock for OBS Studio or vMix
-**  Copyright (c) 2021-2023 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2021-2024 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
